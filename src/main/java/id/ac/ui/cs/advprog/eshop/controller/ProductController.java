1822ea9205bfb3c6aae6a1a15966bb1c8b13babf
Farrell Muhammad Hanau <hanau.muhammad@gmail.com>
2024-02-07 11:57:55 +0700

    Added edit product feature

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
@@ -35,4 +35,17 @@ public class ProductController {
         model.addAttribute("products", allProduct);
         return "productList";
     }
+
+    @GetMapping("/edit/{id}")
+    public String editProductPage(@PathVariable String id, Model model){
+        Product product = service.find(id);
+        model.addAttribute("product", product);
+        return "editProduct";
+    }
+
+    @PostMapping("/edit")
+    public String editProductPost(@ModelAttribute Product product, Model model){
+        service.edit(product);
+        return "redirect:list";
+    }
 }
