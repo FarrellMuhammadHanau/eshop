1ac3de525ca54a7afc4e27e412d57a259e9d5a0b
Farrell Muhammad Hanau <hanau.muhammad@gmail.com>
2024-03-06 16:54:50 +0700

    [RED] Add PaymentRepository skeleton

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
@@ -0,0 +1,24 @@
+package id.ac.ui.cs.advprog.eshop.repository;
+
+import id.ac.ui.cs.advprog.eshop.model.Order;
+import id.ac.ui.cs.advprog.eshop.model.Payment;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository
+public class PaymentRepository {
+    private List<Payment> paymentData = new ArrayList<>();
+    private Map<String, Order> ordermap = new HashMap<>();
+
+    public Payment save(Payment payment, Order order) {return null;}
+
+    public Payment findById(String id) {return null;}
+
+    public List<Payment> findAll() {return null;}
+
+    public Order findOrderByPaymentId(String id) {return null;}
+}
